539ec950083747c719ea634481b95cc856a24055
harry <hzoeftig@gmail.com>
2024-01-03 17:36:28 +0000

    Added method to get loan end date

--- src/BorrowRecord.java
+++ src/BorrowRecord.java
@@ -2,13 +2,20 @@ public class BorrowRecord extends Status {
     Customer customer;
     Book book;
     Date date;
+    int loanLength;
     public BorrowRecord() {
         super("Borrow Record");
     }
-    public void writeRecord(Customer customer, Book book, Date date) {
+    public void writeRecord(Customer customer, Book book, Date date, int loanLength) {
         this.customer = customer;
         this.book = book;
         this.date = date;
+        this.loanLength = loanLength;
+    }
+    public Date getLoanEndDate() {
+        Date endDate = date.copy();
+        endDate.addDays(loanLength);
+        return endDate;
     }
     public String toString() {
         return book.toString()+"\n"+customer.toString()+"\n"+date.toString();
--- src/Date.java
+++ src/Date.java
@@ -11,4 +11,39 @@ public class Date {
     public String toString(){
         return day+"/"+month+"/"+year;
     }
+    public Date copy() {
+        return new Date(day, month, year);
+    }
+    public void addDays(int numOfDays) {
+        int daysInFebruary = 28;
+        if (year%4 == 0 && (year%400==0 || year%100!=0)) {
+            daysInFebruary = 29;
+        }
+        int newDay = day+numOfDays;
+        int daysInCurrentMonth;
+        if (month == 2) {
+            daysInCurrentMonth = daysInFebruary;
+        } else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+            daysInCurrentMonth = 31;
+        }
+        else {
+            daysInCurrentMonth = 30;
+        }
+        while (newDay>daysInCurrentMonth) {
+            newDay = newDay - daysInCurrentMonth;
+            month++;
+            if (month > 12){
+                year++;
+                month=1;
+            }
+            if (month == 2) {
+                daysInCurrentMonth = daysInFebruary;
+            } else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+                daysInCurrentMonth = 31;
+            }
+            else {
+                daysInCurrentMonth = 30;
+            }
+        }
+    }
 }
