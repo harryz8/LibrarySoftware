b01937cc6738b3a274b4e7bbc8c602ca6efcab8c
harry <hzoeftig@gmail.com>
2024-01-06 19:33:33 +0000

    Added methods to Customer class to add and remove books from account

--- src/Customer.java
+++ src/Customer.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+
 public class Customer {
     String firstName;
     String surname;
     int ID;
+    ArrayList<BorrowRecord> loanedBooks = new ArrayList<>();
     public Customer(String firstName, String surname, int IDNum) {
         this.firstName = firstName;
         this.surname = surname;
@@ -10,5 +13,20 @@ public class Customer {
     @Override
     public String toString(){
         return Integer.toString(ID)+","+firstName+","+surname;
+    }
+    public void addLoanedBook(BorrowRecord record) {
+        loanedBooks.add(record);
+    }
+    public Book removeLoanedBook(BorrowRecord oldRecord, LibraryRecord newRecord) {
+        Book book;
+        for (int i = 0; i<loanedBooks.size(); i++) {
+            if (loanedBooks.get(i).equals(oldRecord)) {
+                book = loanedBooks.get(i).getBook();
+                book.setStatus(newRecord);
+                loanedBooks.remove(i);
+                return book;
+            }
+        }
+        return null;
     }
 }
